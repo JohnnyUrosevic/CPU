f43da6ebb76269ef253e95c43ebf0158a36068b8
Johnny Urosevic <TheMagicalCake@gmail.com>
2018-08-22 00:08:37 -0700

    Error checking for mismatched length bits

--- gates.hs
+++ gates.hs
@@ -1,6 +1,8 @@
 type Bits = [Bool]
 
 nor :: Bits -> Bits -> Bits
+nor [] (_:__) = error "mismatched length"
+nor (_:__) [] = error "mismatched length"
 nor [] [] = []
 nor (x:xs) (y:ys) = (norBit x y):nor xs ys
     where norBit x y = if x == True || y == True then False else True
@@ -9,4 +11,4 @@ not :: Bits -> Bits
 not x = nor x x
 
 main = do
-    print True
+    print (nor [True] [False, True])
