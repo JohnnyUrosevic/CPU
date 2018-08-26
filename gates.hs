00763b14e97c4dfefb6b7a4804b76d554cb715bb
Johnny Urosevic <TheMagicalCake@gmail.com>
2018-08-25 22:14:46 -0700

    XOR

--- gates.hs
+++ gates.hs
@@ -11,13 +11,16 @@ not :: Bits -> Bits
 not x = nor x x
 
 or :: Bits -> Bits -> Bits
-or x y = Main.not (nor x y)
+or x y = Main.not $ nor x y
 
 and :: Bits -> Bits -> Bits
 and x y = nor (Main.not x) (Main.not y)
 
 nand :: Bits -> Bits -> Bits
-nand x y = Main.not (Main.and x y)
+nand x y = Main.not $ Main.and x y
+
+xor :: Bits -> Bits -> Bits
+xor x y = Main.and (Main.or x y) (Main.nand x y)
 
 main = do
-    print (Main.and [True, False, True] [False, False, True])
+    print (Main.xor [True, False, True] [False, False, True])
